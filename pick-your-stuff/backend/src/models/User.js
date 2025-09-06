2ec04a26540b218c01af17c5fac3a241139b357b
Stoyan Minchev <154098583+StoyanMinc@users.noreply.github.com>
2025-09-06 20:00:53 +0300

    Create User model

---
+++ pick-your-stuff/backend/src/models/User.js
@@ -0,0 +1,25 @@
+import { Schema, model } from 'mongoose';
+import bcrypt from 'bcrypt';
+
+const userSchema = new Schema({
+    username: {
+        type: String,
+        required: [true, 'Username is required!']
+    },
+    password: {
+        type: String,
+        required: [true, 'Password is required!']
+    },
+}, { timestamps: true });
+
+userSchema.pre('save', async function (next) {
+    if (!this.isModified('password')) {
+        return next();
+    }
+    const hashedPassword = await bcrypt.hash(this.password, 10);
+    this.password = hashedPassword;
+    next();
+})
+const User = model('User', userSchema);
+
+export default User;
