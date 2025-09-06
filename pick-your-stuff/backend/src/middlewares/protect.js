f0c4be4d32b20ed8ce6e6c1cfe6140241b8a98f1
Stoyan Minchev <154098583+StoyanMinc@users.noreply.github.com>
2025-09-06 20:03:10 +0300

    Implement protect middleware

---
+++ pick-your-stuff/backend/src/middlewares/protect.js
@@ -0,0 +1,30 @@
+import User from "../models/User.js";
+import { verifyAccessToken } from "../utils/token.js";
+
+export const protect = async (req, res, next) => {
+    try {
+        const accessToken = req.headers.authorization;
+        if (!accessToken) {
+            return res.status(401).json({ message: 'Not authorized, please login!' });
+        }
+        const decodedToken = verifyAccessToken(accessToken);
+        if (!decodedToken) {
+            return res.status(403).json({ message: 'Invalid token! Please login again!' });
+        }
+        const user = await User.findById(decodedToken.id).select('-password');
+        if (!user) {
+            return res.status(404).json({ message: 'User not found!' });
+        }
+        req.user = user;
+        next();
+    } catch (error) {
+        if (error.name === "TokenExpiredError") {
+            return res.status(401).json({ message: "Access token expired" });
+        }
+        if (err.name === "JsonWebTokenError") {
+            return res.status(403).json({ message: "Invalid token" });
+        }
+        console.log('ERROR PROTECT MIDDLEWARE:', error);
+        res.status(500).json({ message: 'Internal server error!' });
+    }
+};
