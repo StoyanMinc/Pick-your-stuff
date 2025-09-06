f51748e2f6d5667ba775740fe3b8d3a6b3cf405d
Stoyan Minchev <154098583+StoyanMinc@users.noreply.github.com>
2025-09-06 20:00:19 +0300

    Implement jwt util

---
+++ pick-your-stuff/backend/src/utils/token.js
@@ -0,0 +1,22 @@
+import jwt from 'jsonwebtoken';
+
+export function generateAccessToken(id) {
+    const token = jwt.sign({ id }, process.env.JWT_SECRET_ACCESS, {
+        expiresIn: '15m'
+    });
+    return token;
+};
+
+export function generateRefreshToken(id) {
+    const token = jwt.sign({ id }, process.env.JWT_SECRET_REFRESH, {
+        expiresIn: '7d'
+    });
+    return token;
+};
+export function verifyAccessToken(token) {
+    return jwt.verify(token, process.env.JWT_SECRET_ACCESS)
+}
+
+export function verifyRefreshToken(token) {
+    return jwt.verify(token, process.env.JWT_SECRET_REFRESH)
+}
