a94627539fca12bd4b3e183a5da987427b3fecb4
Stoyan Minchev <154098583+StoyanMinc@users.noreply.github.com>
2025-09-10 07:59:48 +0300

    Implement email token util

--- pick-your-stuff/backend/src/utils/token.js
+++ pick-your-stuff/backend/src/utils/token.js
@@ -13,10 +13,22 @@ export function generateRefreshToken(id) {
     });
     return token;
 };
+
+export function generateEmailToken(listId, email) {
+    const token = jwt.sign({ listId, email }, process.env.JWT_SECRET_EMAIL, {
+        expiresIn: '7d'
+    });
+    return token;
+};
+
 export function verifyAccessToken(token) {
-    return jwt.verify(token, process.env.JWT_SECRET_ACCESS)
-}
+    return jwt.verify(token, process.env.JWT_SECRET_ACCESS);
+};
 
 export function verifyRefreshToken(token) {
-    return jwt.verify(token, process.env.JWT_SECRET_REFRESH)
-}
+    return jwt.verify(token, process.env.JWT_SECRET_REFRESH);
+};
+
+export function verifyEmailToken(token) {
+    return jwt.verify(token, process.env.JWT_SECRET_EMAIL);
+};
