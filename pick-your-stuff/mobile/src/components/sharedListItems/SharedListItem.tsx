1429b5d5dc7355411c56506fad60b29bf5df1222
Stoyan Minchev <154098583+StoyanMinc@users.noreply.github.com>
2025-09-13 23:39:16 +0300

    Create Shared List Items component

---
+++ pick-your-stuff/mobile/src/components/sharedListItems/SharedListItem.tsx
@@ -0,0 +1,119 @@
+import {
+    View,
+    Text,
+    FlatList,
+    TouchableOpacity,
+    Switch,
+    Alert,
+    ActivityIndicator
+} from 'react-native';
+import { useNavigation, useRoute, RouteProp } from '@react-navigation/native';
+import { useEffect } from 'react';
+
+import { styles } from './sharedListItem.styles';
+import { useListItems } from '../../hooks/useListItem';
+
+type RootStackParamList = {
+    'Shared Lists': undefined;
+    List: { id: string; title: string };
+};
+
+type ListRouteProp = RouteProp<RootStackParamList, 'List'>;
+
+export default function SharedListItem() {
+    const route = useRoute<ListRouteProp>();
+    const { id, title } = route.params;
+
+    const {
+        items,
+        loading,
+        error,
+        deleteItem,
+        toggleItem,
+        checkAll,
+        uncheckAll,
+        refresh
+    } = useListItems(id, false);
+
+    const navigation = useNavigation();
+
+    useEffect(() => {
+        if (title) {
+            navigation.setOptions({ title });
+        }
+    }, [navigation, title]);
+
+    const showDeleteConfirmation = (id: string, title: string) => {
+        Alert.alert(
+            'Delete Task',
+            `Are you sure you want to delete "${title}"?`,
+            [
+                { text: 'Cancel', style: 'cancel' },
+                { text: 'Delete', style: 'destructive', onPress: () => deleteItem(id) },
+            ]
+        );
+    };
+
+    if (loading) {
+        return (
+            <View style={[styles.container, { justifyContent: 'center', alignItems: 'center' }]}>
+                <ActivityIndicator size="large" color="#6a0dad" />
+                <Text style={styles.spinnerText}>Loading lists items...</Text>
+            </View>
+        );
+    }
+
+    if (error) {
+        return (
+            <View style={[styles.container, styles.errorContainer]}>
+                <Text style={styles.errorText}>{error}</Text>
+            </View>
+        );
+    }
+
+    return (
+        <View style={styles.container}>
+            <View style={styles.buttonContainer}>
+                <View style={styles.checkButtonsContainer}>
+                    <TouchableOpacity
+                        style={[styles.checkingBtn, styles.checkBtn]}
+                        onPress={() => checkAll(id)}>
+                        <Text style={styles.buttonText}>check all</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity
+                        style={[styles.checkingBtn, styles.uncheckBtn]}
+                        onPress={() => uncheckAll(id)}>
+                        <Text style={styles.buttonText}>uncheck all</Text>
+                    </TouchableOpacity>
+                </View>
+            </View>
+            <View style={{ flex: 1 }}>
+                {items.length > 0 ? (
+                    <FlatList
+                        contentContainerStyle={{ paddingBottom: 80 }}
+                        data={items}
+                        keyExtractor={(item) => item._id}
+                        renderItem={({ item }) => (
+                            <View style={styles.itemView}>
+                                <Switch
+                                    style={styles.switch}
+                                    value={item.isChecked}
+                                    onValueChange={() => { toggleItem(item._id, !item.isChecked) }}
+                                />
+                                <TouchableOpacity onLongPress={() => showDeleteConfirmation(item._id, item.title)}>
+                                    <Text>{item.title}</Text>
+                                </TouchableOpacity>
+                            </View>
+                        )}
+                        refreshing={loading}   // 🔹 shows spinner when loading
+                        onRefresh={refresh}
+                    />
+                ) : (
+                    <View style={styles.noDataContainer}>
+                        <Text style={styles.noDataText}>No data to show...</Text>
+                    </View>
+                )}
+            </View>
+        </View>
+    );
+}
---
+++ pick-your-stuff/mobile/src/components/sharedListItems/sharedListItem.styles.ts
@@ -0,0 +1,99 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        padding: 10
+    },
+    buttonContainer: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        marginBottom: 10,
+    },
+    checkButtonsContainer: {
+        flexDirection: 'row',
+        gap: 10
+    },
+    checkingBtn: {
+        padding: 5,
+        borderRadius: 5
+    },
+    checkBtn: {
+        backgroundColor: 'green',
+    },
+    uncheckBtn: {
+        backgroundColor: 'red',
+    },
+    shareBtn: {
+        backgroundColor: 'yellow',
+    },
+    shareBtnText: {
+        color: '#000'
+    },
+    button: {
+        backgroundColor: 'blue',
+        padding: 10,
+        width: 150,
+        borderRadius: 10,
+    },
+    buttonText: {
+        color: '#fff',
+        fontWeight: 'bold',
+        textAlign: 'center',
+        fontSize: 12
+    },
+    addItemContainer: {
+        gap: 10,
+        alignItems: 'center',
+        marginBottom: 10
+    },
+    input: {
+        borderColor: 'green',
+        borderWidth: 0.2,
+        paddingVertical: 15,
+        paddingHorizontal: 10,
+        width: '100%',
+        backgroundColor: '#fff',
+        borderRadius: 10,
+    },
+    confirmButton: {
+        backgroundColor: 'green'
+    },
+    itemView: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        gap: 10,
+        padding: 5,
+        backgroundColor: '#fff',
+        borderBottomWidth: 1,
+        borderColor: '#ccc'
+    },
+    switch: {
+        transform: [{ scaleX: 0.7 }, { scaleY: 0.7 }]
+    },
+    noDataContainer: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    noDataText: {
+        fontWeight: 'bold',
+        fontSize: 25,
+        color: '#6a0dad'
+    },
+    spinnerText: {
+        marginTop: 10,
+        fontSize: 20,
+        fontWeight: 'bold'
+    },
+    errorContainer: {
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    errorText: {
+        color: 'red',
+        fontWeight: 'bold',
+        fontSize: 16
+    }
+})
