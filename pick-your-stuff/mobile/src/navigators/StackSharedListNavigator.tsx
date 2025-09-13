4fe230b6d4bba97182e9361c6d9de9d78330a0de
Stoyan Minchev <154098583+StoyanMinc@users.noreply.github.com>
2025-09-13 23:43:01 +0300

    Create StackSharedListNavigator.tsx

---
+++ pick-your-stuff/mobile/src/navigators/StackSharedListNavigator.tsx
@@ -0,0 +1,34 @@
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { RootStackParamList } from '../types';
+import SharedLists from '../components/sharedLists/SharedLists';
+import SharedListItem from '../components/sharedListItems/SharedListItem';
+import AcceptList from '../components/acceptList/AcceptList';
+import DeclineList from '../components/declineList/DeclineList';
+
+const StackNavigator = createNativeStackNavigator<RootStackParamList>();
+
+export default function StackSharedListNavigator() {
+    return (
+        <StackNavigator.Navigator
+            screenOptions={{
+                headerTintColor: '#6a0dad',
+                headerTitleStyle: {
+                    fontWeight: 'bold',
+                },
+            }}
+        >
+            <StackNavigator.Screen name="Shared Lists" component={SharedLists} />
+            <StackNavigator.Screen name="Shared List" component={SharedListItem} />
+            <StackNavigator.Screen
+                name="AcceptList"
+                component={AcceptList}
+                options={{ headerShown: false }}
+            />
+            <StackNavigator.Screen
+                name="DeclineList"
+                component={DeclineList}
+                options={{ headerShown: false }}
+            />
+        </StackNavigator.Navigator>
+    );
+}
